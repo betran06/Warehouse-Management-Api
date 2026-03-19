dbee2a9da25d0346c9174761befbbadcb6626922
unknown <betranarya03@gmail.com>
2026-03-19 23:50:15 +0700

    model commit

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -3,9 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Merchant extends Model
 {
     //
-    use SoftDeletes
+    use SoftDeletes;
+
+    protected $filllable = ['name', 'address', 'photo', 'phone', 'keeper_id'];
+
+    public function keeper()
+    {
+        return $this->belongsTo(User::class, 'keeper_id');    
+    }
+
+    public function products() //untuk mengecek ditoko tersebut ada produk apa saja
+    {
+        return $this->belongToMany(Products::class, 'merchant_products')
+        ->withPivot("stock")
+        ->withPivot('warehouse_id')
+        ->withTimestamps();
+    }
+
+    public function transactions() // ngecek ditoko ini ada berapa transaksi
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        if (!$value) {
+            return null; // no image available
+        }
+
+        return url(Storage::url(!$value));
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Soft;
 
 class Transaction extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'phone', 'sub_total', 'tax_total', 'grand_total', 'merchant_id'];
+
+    public function merchant() //transaksi ini dimiliki toko yang mana
+    {
+        return $this->belongsTo(Merchant::class);    
+    }
+
+    public function transactionProducts() //dalam satu transaksi ada produk apa saja yang dibeli
+    {
+        return $this->hasMany(TransactionProduct::class);
+    }
 }
